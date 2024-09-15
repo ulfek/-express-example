9e98ababf9b4aa155af5b2bbcf60c753e0939b92
ulfekl <ulfekl@gmail.com>
2024-09-15 19:36:33 +0200

    modified:   server.js

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = process.env.PORT || 3000; //Set port to env-variable or use 3000
 
 app.get('/', (req, res) => {
     res.send('Successful response.');
 });
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    });
+// app.listen(3000, () => console.log('Example app is listening on port 3000.'));
